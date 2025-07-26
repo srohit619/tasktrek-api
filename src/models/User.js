c979c9849f077e124be39927a867c48b63a8cb30
Rohit Shetty <rohitshetty@credenceanalytics.com>
2025-07-26 19:02:08 +0530

    Phase 3: Complete user login with JWT generation

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs'); // Import bcrypt
+const jwt = require('jsonwebtoken'); // Import jsonwebtoken
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,6 +22,18 @@ const userSchema = new mongoose.Schema({
   },
 }, { timestamps: true });
 
+// Add this new method to the user schema
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET, { expiresIn: '7 days' });
+
+  // In a real app, you might want to save the token to the user model to manage sessions
+  // user.tokens = user.tokens.concat({ token });
+  // await user.save();
+
+  return token;
+};
+
 // Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
   const user = this;
@@ -31,6 +44,7 @@ userSchema.pre('save', async function (next) {
 
   next();
 });
+
 
 const User = mongoose.model('User', userSchema);
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const User = require('../models/User');
+const bcrypt = require('bcryptjs'); // Import bcrypt here
+
 const router = new express.Router();
 
 // -- SIGNUP --
@@ -11,6 +13,30 @@ router.post('/users', async (req, res) => {
     res.status(201).send({ user });
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+// -- LOGIN --
+router.post('/users/login', async (req, res) => {
+  try {
+    // 1. Find the user by email
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).send({ error: 'Unable to login' });
+    }
+
+    // 2. Compare the incoming password with the stored hash
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(400).send({ error: 'Unable to login' });
+    }
+
+    // 3. Generate a token if credentials are valid
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+
+  } catch (e) {
+    res.status(500).send();
   }
 });
 
